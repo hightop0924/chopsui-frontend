6554db2a9a365f52e47b43cf442aa68d2d000c2c
Mukai009 <yeti980830@gmail.com>
2023-06-07 23:31:46 -0500

    fix: fixed typo issue

--- src/pages/Home/About.tsx
+++ src/pages/Home/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <div className="bg-primary-200 grid font-rubik  text-white p-5 md:p-20 ">
       <div className="place-self-center grid gap-5 text-center">
-        <h1 className=" text-secondary-50 text-4xl font-bold">WHY CHIOPSUI</h1>
+        <h1 className=" text-secondary-50 text-4xl font-bold">WHY CHOPSUI</h1>
         <p className="text-xl md:text-2xl md:font-bold">
           Earn sustainable yields to compound your holdings
         </p>
