b0ed503a2ff115b3b3432ecb9906b0695547f141
Judeeen <83718263+gyroflaw@users.noreply.github.com>
2023-06-22 02:52:04 -0500

    fix: fixed litepaper link

--- src/pages/Home/components/MyDocument.tsx
+++ src/pages/Home/components/MyDocument.tsx
@@ -27,27 +27,33 @@ export default function MyDocument() {
 
   return (
     <div className="pt-150 bg-blue-300 grid ">
-      <div className="grid place-content-center place-items-center">
-        <Document file={chopsui} onLoadSuccess={onDocumentLoadSuccess}>
+      <div className="grid place-content-center place-items-center mt-32">
+        <Document
+          file={chopsui}
+          className="mt-32"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
           <Page pageNumber={pageNumber} />
         </Document>
       </div>
-      <div className="flex place-content-between">
-        <Button
-          variant="primary"
-          onClick={onBefore}
-          className="text-secondary-50 mt-7 md:w-96 col-span-2  !font-[inter] justify-between md:!px-7 py-3 md:py-5 gap-20 text-base md:text-xl"
-        >
-          Before
-        </Button>
-        <Button
-          onClick={onNext}
-          variant="primary"
-          className="text-secondary-50 mt-7 md:w-96 col-span-2  !font-[inter] justify-between md:!px-7 py-3 md:py-5 gap-20 text-base md:text-xl"
-        >
-          Next
-        </Button>
-      </div>
+      {numPages !== 1 ? (
+        <div className="flex place-content-evenly ">
+          <Button
+            variant="primary"
+            onClick={onBefore}
+            className="text-secondary-50 mt-7 md:w-96 col-span-2  !font-[inter] justify-between md:!px-7 py-3 md:py-5 gap-20 text-base md:text-xl"
+          >
+            Before
+          </Button>
+          <Button
+            onClick={onNext}
+            variant="primary"
+            className="text-secondary-50 mt-7 md:w-96 col-span-2  !font-[inter] justify-between md:!px-7 py-3 md:py-5 gap-20 text-base md:text-xl"
+          >
+            Next
+          </Button>
+        </div>
+      ) : null}
     </div>
   );
 }
