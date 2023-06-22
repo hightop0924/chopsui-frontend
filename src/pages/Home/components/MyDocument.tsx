d9cd4f7750d55aaed46d751664dfe0838194d928
Judeeen <83718263+gyroflaw@users.noreply.github.com>
2023-06-22 01:58:48 -0500

    test

--- src/pages/Home/components/MyDocument.tsx
+++ src/pages/Home/components/MyDocument.tsx
@@ -2,28 +2,22 @@ import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import chopsui from "./chopsui.pdf";
 
-// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-//   "./pdf.worker.js",
-//   import.meta.url
-// ).toString();
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function MyDocument() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(true);
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: any }) {
-    setNumPages(numPages);
+  function onDocumentLoadSuccess() {
+    setLoading(false);
   }
 
   return (
-    <div className="mt-40 ">
-      <Document file={chopsui} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+    <div className="pt-20 bg-blue-300">
+      {loading ? (
+        <Document file={chopsui} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page />
+        </Document>
+      ) : null}
     </div>
   );
 }
